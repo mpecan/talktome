5b8c16bc80df104a38c057748dcc45df706c9fcd
ibrahimbayer <ibrahimbayer@gmail.com>
2017-07-02 21:48:25 +0300

    Test to validate swagger is up & running. Checking swagger.json file exported at path /v2/api-docs

---
+++ src/test/java/org/ibayer/personal/talktome/swagger/TestSwaggerConfig.java
@@ -0,0 +1,34 @@
+package org.ibayer.personal.talktome.swagger;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@SpringBootTest
+@AutoConfigureMockMvc(print = MockMvcPrint.NONE)
+public class TestSwaggerConfig {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void shouldReturnApiDocs() throws Exception {
+		ResultActions result = mockMvc.perform(get("http://localhost:8080/v2/api-docs"));
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.paths").exists());
+		result.andExpect(jsonPath("$.tags").exists());
+	}
+}
